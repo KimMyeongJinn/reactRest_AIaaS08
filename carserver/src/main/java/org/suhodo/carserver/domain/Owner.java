c1e911c80699a2e17bf3e3c70b17dc94ff0481f9
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-09-04 12:42:58 +0900

    일반적인 방식의 RestController만들기 - 주석 추가

--- carserver/src/main/java/org/suhodo/carserver/domain/Owner.java
+++ carserver/src/main/java/org/suhodo/carserver/domain/Owner.java
@@ -29,6 +29,12 @@ public class Owner {
     *  교차 테이블이 생성되게 된다.
     * 1대 N 관계로 정하기 위해 아래처럼 설정한다.
     **/
+    /* CarController에 요청할 때
+      Car에서는 Owner의 데이터도 조회해서 json으로 전송하는데
+      이때 다시 자식인 cars를 조회하는 순환 조회가 일어나므로
+      무한 루프에 빠져 StackOverflow가 일어나면 예외가 발생한다.
+      그러므로 json데이터를 보내지 않는 설정을 해준다.
+    * */
     @JsonIgnore         // json으로 전송할 때 제외한다.
     @OneToMany(mappedBy = "owner",
             cascade = {CascadeType.ALL},
