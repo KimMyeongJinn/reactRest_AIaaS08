2890c8a50b7008071fcf1917bed8b1e402124845
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-09-06 12:33:31 +0900

    서버에 car목록을 요청해서 앱에 출력

---
+++ carfront/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+// import MainApp from "./ex01_main_app/MainApp";
+import MainApp from "./ex02_car_list/MainApp";
+
+function App() {
+  return <MainApp />;
+}
+
+export default App;
---
+++ carfront/src/ex02_car_list/components/CarList.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+function CarList() {
+  const [cars, setCars] = useState([]); // car목록을 서버로부터 가져와서 저장
+
+  // Application이 시작하면 서버에 1번 요청
+  useEffect(() => {
+    fetch("http://localhost:12000/api/cars")
+      .then((response) => response.json())
+      .then((data) => setCars(data._embedded.cars))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          {cars.map((car, index) => {
+            return (
+              <tr key={index}>
+                <td>{car.brand}</td>
+                <td>{car.model}</td>
+                <td>{car.color}</td>
+                <td>{car.year}</td>
+                <td>{car.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default CarList;
